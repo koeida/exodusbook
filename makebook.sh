787812527007756a7f1ade2926a48e53b796872a
Keegan <keegan@twinoaks.org>
2020-11-11 21:15:04 -0500

    stuff

--- makebook.sh
+++ makebook.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-sed 's/marginpar{\([a-zA-ZāēīōūĀĒĪŌŪ \+<$/(),-]*:\)/marginpar{{\\bf \1}/g' exodus.tex > final.tex
+sed 's/marginpar{\([a-zA-ZāēīōūĀĒĪŌŪ \+<$/()\.,-]*:\)/marginpar{{\\bf \1}/g' exodus.tex > final.tex
 #sed 's/ā/=a/g' temp1.tex | sed 's/ē/=e/g' | sed 's/ī/=i/g' | sed 's/ō/=o/g' | sed 's/ū/=u/g' > final.tex
-pdflatex final.tex
+xelatex final.tex
+#pdflatex final.tex
