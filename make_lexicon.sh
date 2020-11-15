8607e33d594fe98ec26cb7d12b40ff67f51b1a30
Keegan <keegan@twinoaks.org>
2020-11-14 20:33:35 -0500

    organize

--- make_lexicon.sh
+++ make_lexicon.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-grep -oP "(?<=marginpar).*?(?=})" exodus.tex
+grep -oP "(?<=marginpar{).*?(?=})" exodus.tex | grep "[:]" | sort -u | sed 's/\([a-zA-ZāēīōūĀĒĪŌŪ \+<$/()\.,-]*:\)/{\\bf \1}/g' | sed 's/$/\n\n/g' > lexicon.tex
