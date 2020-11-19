56980ab5aec1a12df63fc20da1a592e33a453858
Keegan <keegan@twinoaks.org>
2020-11-19 16:42:36 -0500

    cap 7 done

--- makebook.sh
+++ makebook.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 sh make_lexicon.sh 
+strip_macrons < lexicon.tex > lexicon_nomacros.tex
 sed 's/marginpar{\([a-zA-ZāēīōūĀĒĪŌŪ \+<$/()\.,-]*:\)/marginpar{{\\bf \1}/g' exodus.tex > final.tex
 #sed 's/ā/=a/g' temp1.tex | sed 's/ē/=e/g' | sed 's/ī/=i/g' | sed 's/ō/=o/g' | sed 's/ū/=u/g' > final.tex
 xelatex final.tex
