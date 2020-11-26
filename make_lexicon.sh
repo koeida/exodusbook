61910b134546e34bf51a07ec63d27253de307be6
Keegan <keegan@twinoaks.org>
2020-11-25 23:03:56 -0500

    stuff

--- make_lexicon.sh
+++ make_lexicon.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-cat cap*.tex qa*.tex | grep -Pazo "mpp{.*?}{.*?}" | sed 's/\x0/\n/g' | grep -Pa "[:]" | sort -u | sed 's/mpp/\\lexentry/g'  > lexicon.tex
+#cat cap*.tex qa*.tex | grep -Pazo "mpp{.*?}{.*?}" | sed 's/\x0/\n/g' | grep -Pa "[:]" | sort -u | sed 's/mpp/\\lexentry/g' #  > lexicon.tex
+cat cap*.tex qa*.tex | grep -Pazo "mpp{.*?}{.*?}"  | sed 's/\x0/\n/g'  | sort -u | sed 's/mpp/\\lexentry/g' > lexicon.tex
