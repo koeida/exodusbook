455f9e4601eb64c745afddef4f1b19a2f1285dc4
Keegan <keegan@twinoaks.org>
2021-08-01 22:58:34 -0400

    macron fixer coming along

--- fixmacrons.sh
+++ fixmacrons.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
-pcregrep -o1 "(?<=\\mpp\{).*?}{(.*?)}" cap10.tex | head -n 15 > macronfix1.txt
-pcregrep -o1 "(?<=\\mpp\{).*?}{(.*?)}" cap10.tex | head -n 15 | xargs -n1 -I {} echo {} | /home/laptop/prog/latin-macronizer/macronize.py > macronfix2.txt
+# Concat book into one big file
+# Replace \n\n with weird sequence of ~~~~~~
+# Replace \n with space
+cat cap*.tex | tr '\n' '|' | sed 's/||/~~~~~/g' | tr '|' ' ' > everything_all_together.tex 
+pcregrep --buffer-size=1000000 -o1 "(?<=\\mpp\{).*?}{(.*?)}" everything_all_together.tex | sort | uniq > macronfix1.txt
+pcregrep --buffer-size=1000000 -o1 "(?<=\\mpp\{).*?}{(.*?)}" everything_all_together.tex | sort | uniq | xargs -0 -n1 -I {} echo {} | /home/laptop/prog/latin-macronizer/macronize.py > macronfix2.txt
 diff -y macronfix1.txt macronfix2.txt
+
